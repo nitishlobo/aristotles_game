999de612501c6292c82f55961ca1810717cbfbb1
Nitish Lobo <nitishlobo@gmail.com>
2017-07-29 01:34:07 +1200

    Added error message display if dictionary file was found to be missing.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 import linecache
 import colours
 
+#English words dictionary.
+DICTIONARY_FILE = 'dictionary.csv'
+
 class Cell(object):
     '''Rectangular cell with the game word and game properties.
 
@@ -34,12 +37,9 @@ class Cell(object):
 
     def display_word(self):
         '''Display the game word.'''
-        #Use default pygame font type
-        font = pygame.font.Font(None, self.font_size)
         #Get an invisible rectangular surface for the word and configure its location.
-        text_surf = font.render(self.word, True, colours.WHITE)
-        text_rect = text_surf.get_rect()
-        text_rect.center = (self.left + (self.width/2), self.top + (self.height/2))
+        text_surf, text_rect = get_text_surf_and_pos(self.surf, self.word, colours.WHITE, \
+                                                    self.font_size, self.left + (self.width/2), self.top + (self.height/2))
 
         #Overlay current word onto the game display surface and display it.
         self.surf.blit(text_surf, text_rect)
@@ -47,7 +47,7 @@ class Cell(object):
         return
 
 def file_len(fname):
-    '''Return the number of lines a specified file has.
+    '''Return an int for the number of lines a specified file has.
     Otherwise return -1 if specified file does not exist.
     '''
     #For empty files
@@ -64,17 +64,49 @@ def file_len(fname):
             pass
     return i + 1
 
+def get_text_surf_and_pos(surf, string, colour, font_size, x, y, font=None):
+    '''Return a surface object, its location, width and
+    height on which text/string can be displayed on.
+
+    Keyword arguments:
+    surf -- surface to display the string on.
+    string -- string to display onto screen.
+    colour -- colour of the string.
+    font_size -- font size of the string.
+    x -- pixel from the left of the screen on which the middle of the string will be.
+    y -- pixel from the top of the screen on which the vertical middle of the string will be.
+    font -- font type of the string. None uses inbuilt default pygame font. (default None)
+    '''
+    font = pygame.font.Font(font, font_size)
+    #Get an invisible rectangular surface for the word and configure its location.
+    text_surf = font.render(string, True, colour)
+    text_rect = text_surf.get_rect()
+    text_rect.center = (x, y)
+    return text_surf, text_rect
+
+def poll_for_exit():
+    '''Runs an infinite loop waiting for user to exit pygame.'''
+    while True:
+        for event in pygame.event.get():
+            #Exit pygame display and python script
+            if event.type == pygame.QUIT \
+                or ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE)):
+                pygame.quit()
+                quit()
+    return
+
 def game_loop(game_display):
+    '''Main game loop.'''
     exit_game = False
     while exit_game is False:
         for event in pygame.event.get():
             #Exit pygame display and python script
-            if event.type == pygame.QUIT or ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE)):
+            if event.type == pygame.QUIT \
+                or ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE)):
                 pygame.quit()
                 quit()
 
         pygame.display.update()
-        #print pygame.display.get_surface()
     return
 
 #Main code begins
@@ -86,6 +118,21 @@ if __name__ == '__main__':
     window_h = 400
     game_display = pygame.display.set_mode((window_w, window_h), pygame.RESIZABLE)
     game_display.fill(colours.WHITE)
+
+    #Display error if the dictionary file is missing or if there are not enough words.
+    word_count = file_len(DICTIONARY_FILE)
+    error_msg = "Error: Too few words in the dictionary file to play the game!"
+    if word_count < 20:
+        if word_count == -1:
+            error_msg = "Error: could not find the dictionary file!"
+        text_surf, text_rect = get_text_surf_and_pos(game_display, error_msg, colours.PRIMARY_RED, \
+                                                    45, window_w/2, window_h/2)
+
+        #Overlay current message onto the game display surface and display it.
+        game_display.blit(text_surf, text_rect)
+        pygame.display.update()
+        #Wait for user to exit pygame
+        poll_for_exit()
 
     #Grid properties
     rows = 5
@@ -101,11 +148,11 @@ if __name__ == '__main__':
     gap_w = (cell_w*2)/(cols-1)
     gap_h = (cell_h*2)/(rows-1)
 
-    print("dictionary.csv file has", file_len("dictionary.csv"), "lines")
+    print("dictionary.csv file has", file_len(DICTIONARY_FILE), "lines")
     print("empty.csv file has", file_len("empty.csv"), "lines")
     print("yellow.csv file has", file_len("yellow.csv"), "lines")
 
-    line = linecache.getline("dictionary.csv", 20278)
+    line = linecache.getline(DICTIONARY_FILE, 20278)
     print(line)
 
     word = 'hello'
